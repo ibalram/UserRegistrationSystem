247ce0fd7e34470c8c5ea989c6f342a684a3851b
ibalram <balramsinghrathore3@gmail.com>
2020-09-28 22:43:16 +0530

    Added: Enter a valid first name

--- src/com/cg/UserRegistration.java
+++ src/com/cg/UserRegistration.java
@@ -8,6 +8,16 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		System.out.println("User Registration Program with regex validation");
+		String firstName;
+		String namePattern = "^[A-Z]{1}[A-Za-z]{2,}";
+		boolean match = false;
+		System.out.println("Enter the first name:");
+		while (!match) {
+			firstName = sc.nextLine();
+			match = Pattern.matches(namePattern, firstName);
+			if (!match)
+				System.out.println("Invalid First Name, Please enter again (First letter capital, minimum 3 letters)");
+		}
+		System.out.println("First Name is added successfully");
 	}
-
 }
