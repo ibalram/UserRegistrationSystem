caefd858bb18118e336034fb17d1907338118bc2
ibalram <balramsinghrathore3@gmail.com>
2020-09-28 22:52:32 +0530

    Added: Enter a valid last name

--- src/com/cg/UserRegistration.java
+++ src/com/cg/UserRegistration.java
@@ -9,7 +9,9 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("User Registration Program with regex validation");
 		String firstName;
+		String lastName;
 		String namePattern = "^[A-Z]{1}[A-Za-z]{2,}";
+
 		boolean match = false;
 		System.out.println("Enter the first name:");
 		while (!match) {
@@ -18,6 +20,16 @@ public class UserRegistration {
 			if (!match)
 				System.out.println("Invalid First Name, Please enter again (First letter capital, minimum 3 letters)");
 		}
-		System.out.println("First Name is added successfully");
+		System.out.println("First Name is added successfully\n");
+
+		match = false;
+		System.out.println("Enter the last name:");
+		while (!match) {
+			lastName = sc.nextLine();
+			match = Pattern.matches(namePattern, lastName);
+			if (!match)
+				System.out.println("Invalid Last Name, Please enter again (First letter capital, minimum 3 letters)");
+		}
+		System.out.println("Last Name is added successfully\n");
 	}
 }
