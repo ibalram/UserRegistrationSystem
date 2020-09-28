dacd5b278e042442dce9e5fcf027d2d03a54ad9a
ibalram <balramsinghrathore3@gmail.com>
2020-09-29 00:45:38 +0530

    Added: Enter phone number in predefined format

--- src/com/cg/UserRegistration.java
+++ src/com/cg/UserRegistration.java
@@ -11,8 +11,10 @@ public class UserRegistration {
 		String firstName;
 		String lastName;
 		String email;
+		String phoneNumber;
 		String namePattern = "^[A-Z]{1}[A-Za-z]{2,}";
 		String emailPattern = "^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$";
+		String phoneNumberPattern = "^[0-9]{1,}[ ][1-9][0-9]{9}$";
 
 		boolean match = false;
 		System.out.println("Enter the first name:");
@@ -33,15 +35,25 @@ public class UserRegistration {
 				System.out.println("Invalid Last Name, Please enter again (First letter capital, minimum 3 letters)");
 		}
 		System.out.println("Last Name is added successfully\n");
-		
+
 		match = false;
 		System.out.println("Enter the email:");
 		while (!match) {
 			email = sc.nextLine();
 			match = Pattern.matches(emailPattern, email);
 			if (!match)
-				System.out.println("Invalid email, Please enter again (First letter capital, minimum 3 letters)");
+				System.out.println("Invalid email, Please enter a valid email)");
 		}
 		System.out.println("Email is added successfully\n");
+
+		match = false;
+		System.out.println("Enter the Phone Number:");
+		while (!match) {
+			phoneNumber = sc.nextLine();
+			match = Pattern.matches(phoneNumberPattern, phoneNumber);
+			if (!match)
+				System.out.println("Invalid phone Number, Please enter a valid phone Number)");
+		}
+		System.out.println("Phone Number is added successfully\n");
 	}
 }
