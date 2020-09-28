bcf81e7366c338c3945b79b71920b473a002223f
ibalram <balramsinghrathore3@gmail.com>
2020-09-28 22:25:39 +0530

    Added: Master branch

---
+++ src/com/cg/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class UserRegistration {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("User Registration Program with regex validation");
+	}
+
+}
